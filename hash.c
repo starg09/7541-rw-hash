0ea162354324db9e316e3bf935d68956068e0835
starg09 <ultraguille2006@gmail.com>
2015-10-06 14:56:38 -0300

    Todo funcionando en este punto
    - Añadida una referencia constante al hash en estructura del iterador,
    para uso interno.
    - Añadido "recorridos" en estructura del iterador, marca cuantas claves
    se revisaron (para optimización de ciclos)
    - Añadidas condiciones faltantes de destrucción de datos de los nodos en
    "hash_borrar".
    - Añadida limpieza automática de listas vacías al borrar sus últimas
    claves.
    - Agregada función interna para avanzar el iterador por el vector.
    - Implementadas todas las funciones del iterador de hash.
    - Implementada la función de destrucción del hash.
    
    Este código en este punto pasa todas las pruebas ofrecidas por la
    cátedra, y no pareciera generar perdidas de memoria.

--- hash.c
+++ hash.c
@@ -24,8 +24,10 @@ typedef struct nodo_hash {
 } nodo_hash_t;
 
 struct hash_iter {
+	const hash_t* hash;
 	lista_iter_t* act;
 	size_t pos_actual;
+	size_t recorridos;
 };
 
 //Duplica una clave, o devuelve NULL de no poder hacerlo.
@@ -166,6 +168,8 @@ bool hash_guardar(hash_t *hash, const char *clave, void *dato){
 			nodo_actual = lista_iter_ver_actual(iter);
 			if (strcmp(nodo_actual->clave, clave) == 0){
 				encontrado = true;
+				if (hash->func_destruct != NULL)
+					hash->func_destruct(nodo_actual->dato);
 				nodo_actual->dato = dato;
 				lista_iter_destruir(iter);
 				return true;
@@ -223,8 +227,13 @@ void *hash_borrar(hash_t *hash, const char *clave){
 				nodo_hash_t* elem = (lista_borrar(hash->vector->datos[pos], iter));
 				lista_iter_destruir(iter);
 				void* dato = elem->dato;
+				free(elem->clave);
 				free(elem);
 				hash->cantidad--;
+				if (lista_esta_vacia(hash->vector->datos[pos])){
+					lista_destruir(hash->vector->datos[pos], NULL);
+					hash->vector->datos[pos] = NULL;
+				}
 				return dato;
 			} else {
 				lista_iter_avanzar(iter);
@@ -264,32 +273,121 @@ void *hash_obtener(const hash_t *hash, const char *clave){
 	}
 }
 
+//Avanza el iterador del hash a través del vector, hasta encontrar o bien ĺa próxima lista existente, o bien el final.
+bool hash_iter_avance_interno(hash_iter_t* iter, size_t* i, lista_iter_t** tempact){
+	while ( ( *tempact == NULL ) && ( *i < ( iter->hash->vector->tam ) ) ) {
+		if (iter->hash->vector->datos[*i] != NULL){
+			*tempact = lista_iter_crear(iter->hash->vector->datos[*i]);
+			if (*tempact == NULL)
+				return false;
+			if (lista_iter_al_final(*tempact)) {
+				lista_iter_destruir(*tempact);
+				*tempact = NULL;
+			}
+		} else {
+			(*i)++;
+		}
+	}
+	return true;
+}
+
 hash_iter_t *hash_iter_crear(const hash_t *hash){
-	//...
-	return NULL;
+	hash_iter_t* hiter = malloc(sizeof(hash_iter_t));
+	if (hiter == NULL)
+		return NULL;
+
+	//Valores caso base, hash vacío
+	hiter->hash = hash;
+	hiter->act = NULL;
+	hiter->pos_actual = 0;
+	hiter->recorridos = 0;
+
+	//Se inicializa de tener elementos
+	if (hash->cantidad > 0) {
+		size_t i = 0;
+		lista_iter_t* tempact = NULL;
+
+		if (!hash_iter_avance_interno(hiter, &i, &tempact)){
+			free(hiter);
+			return NULL;
+		} else {
+			hiter->act = tempact;
+			hiter->pos_actual = i;
+			hiter->recorridos++;
+		}
+	}
+
+	return hiter;
 }
 
 bool hash_iter_avanzar(hash_iter_t *iter){
-	//...
-	return false;
+	if (hash_iter_al_final(iter))
+		return false;
+	if (!lista_iter_avanzar(iter->act))
+		return false;
+	iter->recorridos++;
+	if (lista_iter_al_final(iter->act)) {
+
+		size_t i = iter->pos_actual + 1;
+		lista_iter_t* tempact = NULL;
+
+		if (hash_iter_avance_interno(iter, &i, &tempact)){
+			lista_iter_destruir(iter->act);
+			iter->act = tempact;
+			iter->pos_actual = i;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	return true;
 }
 
 const char *hash_iter_ver_actual(const hash_iter_t *iter){
-	//...
-	return NULL;
+	if (hash_iter_al_final(iter)) {
+		return NULL;
+	} else {
+		lista_iter_t* iter_act = iter->act;
+		nodo_hash_t* nodo_act = lista_iter_ver_actual(iter_act);
+		return nodo_act->clave;
+	}
 }
 
 bool hash_iter_al_final(const hash_iter_t *iter){
-	//...
+	if ((iter->hash->cantidad == 0) || (iter->hash->cantidad < iter->recorridos))
+		return true;
 	return false;
 }
 
 void hash_iter_destruir(hash_iter_t* iter){
-	//...
-	return;
+	if (iter->act != NULL)
+		lista_iter_destruir(iter->act);
+	free(iter);
 }
 
 
+
+
 void hash_destruir(hash_t *hash){
-	return;
+	size_t elems_a_borrar = hash->cantidad;
+	lista_t* lista_actual;
+	nodo_hash_t* elem_actual;
+	int i = 0;
+	while ( ( elems_a_borrar > 0 ) && ( i < (hash->vector->tam) ) ) {
+		lista_actual = hash->vector->datos[i];
+		if (lista_actual != NULL){
+			while (!lista_esta_vacia(lista_actual)){
+				elem_actual = lista_borrar_primero(lista_actual);
+				free(elem_actual->clave);
+				if (hash->func_destruct != NULL)
+					hash->func_destruct(elem_actual->dato);
+				free(elem_actual);
+				elems_a_borrar--;
+			}
+			lista_destruir(lista_actual, NULL); //No uso solo esto con una función auxiliar así puedo detectar si ya borré todos los elementos.
+		}
+		i++;
+	}
+	vector_destruir(hash->vector);
+	free(hash);
 }
